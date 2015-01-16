f9887d0e934f7e319f5d2a7e8404ed12c1d1ef3d
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 12:28:43 -0500

    Added enum initial code

---
+++ ch11/enum/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// create an enum (enumeration)
+
+int main()
+{
+
+  // create an object using that enum
+
+  // enum comparison to int
+
+  // setting an enum to an int!
+
+  // loops with enums
+
+}
