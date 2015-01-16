deeaef4c8b78196076b611264511bf71dd3c6ff6
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 11:12:56 -0500

    Added documentation

--- commandLine/main.cpp
+++ commandLine/main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char* argv[])
   // argc is the number of arguments
   std::cout << "this program has these arguments" << std::endl;
   for (int i = 0; i < argc; ++i)
+    // use argv[i] for the ith argument
     std::cout << argv[i] << std::endl;
 }
