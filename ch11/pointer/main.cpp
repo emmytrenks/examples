42234a0469edb7e5501a493b02c886cfd44324c0
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 12:09:22 -0500

    Added starting code for pointer example

---
+++ ch11/pointer/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+struct Person
+{
+
+};
+
+struct Student
+{
+  // has a pointer to a Person object
+
+};
+
+int main()
+{
+  // create a Person object
+
+  
+  // create a Person pointer
+
+  
+  // create a Student object using the Person pointer
+
+
+  // create a Student pointer
+
+
+  // using the person object 
+
+
+  // using the person pointer
+
+
+  // using the student object
+
+
+  // using the student pointer
+
+
+
+}
