326af14ed2a0615f5d610dc951c668a72f2dabd8
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 12:22:07 -0500

    Added initial union files

---
+++ ch11/unions/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+  // define a union
+  union MyUnion
+  {
+
+  };
+
+  
+  // create a union instance
+
+  
+  // set the smaller value
+
+
+  // set the larger value
+
+
+  // set the smaller value
+
+
+  // create an anonymous union
+
+  /*
+  union
+  {
+
+  };
+  */
+
+  // create a union instance
+
+  
+  // set the smaller value
+
+
+  // set the larger value
+
+
+  // set the smaller value
+
+
+  // create an anonymous union
+ 
+}
