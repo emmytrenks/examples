aa5a937b8ee27991546b7d5b86bfe115930f586c
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 11:23:44 -0500

    Changed the documentation placement in main.cpp and uncommented the initial code

--- ch11/game/main.cpp
+++ ch11/game/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 {
   // create a Game object
   Game g;
-  /*
+  
   // input game information
   std::cout << "Enter the title: ";
   getline(std::cin, g.title);
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
   std::cout << g.playerBase << std::endl;
 
   // make an array of games
-  */
+  
   Game games[10];
 
   games[0].title = "first game";
@@ -76,11 +76,15 @@ int main(int argc, char* argv[])
 	    << (compareGames(games[0], games[1]) ? "" : "not")
 	    << "equal" << std::endl;
 
-  // to compare two instances of a structure
-  // this does not work
-  //if (games[0] == games[1])
-  // if (games[0].title == games[1].title && games[0].developer == games[1].developer && ....  
 }
+
+// to compare two instances of a structure
+
+// this does not work
+// if (games[0] == games[1])
+
+// this is how you need to do it
+// if (games[0].title == games[1].title && games[0].developer == games[1].developer && ....  
 
 bool compareGames(const Game& a, const Game& b)
 {
