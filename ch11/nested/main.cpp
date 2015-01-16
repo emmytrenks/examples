c8faa509c1a08bfdc78598824f9faace0af06deb
Anthony Deeter <aed27@zips.uakron.edu>
2015-01-16 11:52:34 -0500

    Added starting code for nested example

---
+++ ch11/nested/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+struct Person
+{
+  // we can declare multiple variables of the same type by separating them with a ','
+
+};
+
+struct Student
+{
+
+
+};
+
+int main()
+{
+  // create a Person object
+
+  // create a Student object using that Person
+
+  // create both at the same time
+
+}
+
+
+
